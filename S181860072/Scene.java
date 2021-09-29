91e6f9ee8829b74044ef855f1a84d4da17f95e23
fiyqkrc <fiyqkrc@gmail.com>
2021-09-29 18:38:44 +0800

    task 2

--- S181860072/Scene.java
+++ S181860072/Scene.java
@@ -3,19 +3,15 @@ package S181860072;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
+
 
 public class Scene {
 
     public static void main(String[] args) throws IOException {
-
-        Line line = new Line(7);
-        line.put(new Monster(112,23,34), 6);
-        line.put(new Monster(112, 78, 128), 3);
-        line.put(new Monster(78, 23, 17), 1);
-        line.put(new Monster(178, 23, 17), 5);
-        line.put(new Monster(78, 123, 17), 2);
-        line.put(new Monster(78, 23, 117), 4);
-        line.put(new Monster(78, 93, 17), 0);
+        int length=512;
+        Line line = new Line(length);
+        Scene.createMonsters(line,length);
 
         Snake theSnake = Snake.getTheSnake();
 
@@ -31,6 +27,38 @@ public class Scene {
         writer.flush();
         writer.close();
 
+    }
+
+    private static void createMonsters(Line line,int num){
+        int[] place = new int[num]; 
+        for(int i:place)
+            place[i] = 0;
+        int[] rgb={0,256,0};
+        String[] changeRule={"bu","gd","ru","bd","gu","rd"};
+        int deep = 1536/num;
+        for(int i=0;i<num;i++){
+            String action = changeRule[(i/(num/6))%6];
+            int index=0;
+            if(action.charAt(0)=='b'){
+                index=2;
+            }
+            else if(action.charAt(0)=='g')
+                index=1;
+            else 
+                index=0;
+            if(action.charAt(1)=='u'){
+                rgb[index]=(rgb[index]+deep)%256;
+            }
+            else
+                rgb[index]=(rgb[index]-deep)%256;
+            Monster monster = new Monster(rgb[0], rgb[1], rgb[2]);
+            int rand = (new Random()).nextInt(num);
+            while(place[rand%num]!=0){
+                rand++;
+            }
+            line.put(monster,rand%num);
+            place[rand%num]=1;
+        }
     }
 
 }
